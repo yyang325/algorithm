47259aafa10935a9b374f7801f5d87e53826d797
yyang325 <frank.young325@gmail.com>
2016-05-29 14:15:00 -0400

    shortestWordDistance -- use array to store indexs, and compare two index with two pointer.

---
+++ src/shortestWordDistance/Solution.java
@@ -0,0 +1,21 @@
+package shortestWordDistance;
+
+public class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int min = Integer.MAX_VALUE;
+        Integer i1 = null, i2 = null;
+        
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1)){
+                i1 = i;
+            }
+            if(words[i].equals(word2)){
+                i2 = i;
+            }
+            if(i1 != null && i2 != null){
+                min = Math.min(min, Math.abs(i1-i2));
+            }
+        }
+        return min;
+    }
+}
