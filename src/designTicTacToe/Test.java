a827a96341b294320133d31bfaa3240bc9d1791f
yyang325 <frank.young325@gmail.com>
2016-06-08 10:11:43 -0400

    get rid of warning

--- src/designTicTacToe/Test.java
+++ src/designTicTacToe/Test.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Test {
 	public static void main(String[] args){
 		TicTacToe ttt = new TicTacToe(5);
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		int player = -1;
 		int res = 0;
